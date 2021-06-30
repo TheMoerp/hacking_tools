d88a887d447d2e1b15fc1f939f49902a9c977018
moerp <matthias.hottgenroth@icloud.com>
2021-06-30 23:55:12 +0200

    copy to clipboard option

--- adapt_bytestream.py
+++ adapt_bytestream.py
@@ -1,11 +1,13 @@
 import argparse
 import textwrap
+import pyperclip
 
 parser = argparse.ArgumentParser(description='Little tool to help building exploits')
-parser.add_argument('bytestream', help='Bytestream you want to adapt')
+parser.add_argument('bytestream', help='bytestream you want to adapt. acceptable formats: 0xnn...nn, nn..nn, \\xnn...\\xnn')
 parser.add_argument('-s', '--swap', action="store_true", help='swap endianness')
 parser.add_argument('-d', '--delimiter', action="store_true", help='add \\x delimiter')
 parser.add_argument('-r', '--remove', action="store_true", help='remove \\x delimiter')
+parser.add_argument('-c', '--copy', action="store_true", help='copy the adapted bytestream to the clipboard')
 args = parser.parse_args()
 
 byte_stream = args.bytestream
@@ -40,3 +42,7 @@ else:
 
 
 print(f"\nAdapted bytestream: {byte_stream}\n")
+
+if args.copy:
+    pyperclip.copy(byte_stream)
+    print("The adapted bytestream has been copied to the clipboard\n")
