d38f94ef24f893d26299a3e63f7717898e35b9c9
moerp <matthias.hottgenroth@icloud.com>
2021-06-30 22:30:10 +0200

    small adaptions

--- adapt_bytestream.py
+++ adapt_bytestream.py
@@ -5,12 +5,12 @@ parser = argparse.ArgumentParser(description='Little tool to help building explo
 parser.add_argument('bytestream', help='Bytestream you want to adapt')
 parser.add_argument('-s', '--swap', action="store_true", help='swap endianness')
 parser.add_argument('-d', '--delimiter', action="store_true", help='add \\x delimiter')
-parser.add_argument('-r', '--remove_delimiter', action="store_true", help='remove \\x delimiter')
+parser.add_argument('-r', '--remove', action="store_true", help='remove \\x delimiter')
 args = parser.parse_args()
 
 byte_stream = args.bytestream
 
-if args.delimiter and args.remove_delimiter:
+if args.delimiter and args.remove:
     print("\nYou cannot add and remove the delimiter at the same time\n")
     exit()
 
@@ -23,7 +23,7 @@ if "\\x" in byte_stream:
 byte_list = []
 if not del_check:
     if (len(byte_stream) % 2) == 1:
-        byte_stream.insert(0, '0')
+        byte_stream = f"0{byte_stream}"
     byte_list = textwrap.wrap(byte_stream, 2)
 else:
     byte_list = byte_list = byte_stream[2:].split('\\x')
@@ -31,7 +31,7 @@ else:
 if args.swap:
     byte_list.reverse()
 
-if args.delimiter or (del_check and not args.remove_delimiter):
+if args.delimiter or (del_check and not args.remove):
     byte_stream = '\\x' + '\\x'.join(byte_list)
 else:
     byte_stream = ''.join(byte_list)
